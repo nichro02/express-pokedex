3b226960600b0935cf3c1daf546fb0b709b77da8
nichro02 <nichro02@gmail.com>
2020-10-24 17:25:23 -0700

    Added route to fetch Pokemon data when searching by name

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const db = require('../models');
 const router = express.Router();
+const axios = require('axios');
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
@@ -18,6 +19,39 @@ router.post('/', function(req, res) {
   db.pokemon.create(req.body)
   .then(createdFave => {
     res.redirect('/pokemon')
+  })
+});
+/*
+router.post('/faves', (req, res) => {
+  let newFavorite = req.body
+  console.log(newFavorite)
+  db.fave.findOrCreate ({
+      where: {title: newFavorite.title},
+      defaults: {imdbid: newFavorite.imdbid}
+  })
+  .then(([createdFave, wasCreated]) => {
+      res.redirect('/movies/faves')
+  })
+})
+*/
+//GET /pokemon/:id route
+//render show page about a pokemon by passing the pokemon's name to the PokeAPI
+router.get('/:name', function(req, res) {
+  const pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+  //console.log(req.params.name)
+  
+  axios.get(`http://pokeapi.co/api/v2/pokemon/${req.params.name}`)
+  .then(function (apiResponse) {
+    const data = {}
+    //list of characteristics to be added to data object
+    data.name = apiResponse.data.name;
+    //const pokemonName = apiResponse.data.name;
+    //console.log('IS THERE A NAME? ',pokemonName)
+    console.log('here is the pokemon info: ',apiResponse.data)
+    res.render('show', data);
+  })
+  .catch(err => {
+    console.log(err)
   })
 });
 
